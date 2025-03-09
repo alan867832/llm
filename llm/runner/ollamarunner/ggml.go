306fb0f61cbd61486b42056458923ba27f24ebf2
aa <aa@aa.com>
2025-03-09 12:45:09 +0800

    add

---
+++ llm/runner/ollamarunner/ggml.go
@@ -0,0 +1,5 @@
+//go:build ggml
+
+package ollamarunner
+
+import _ "github.com/ollama/ollama/ml/backend/ggml"
--- opt.sh
+++ opt.sh
@@ -6,10 +6,12 @@ case $1 in
   install)
     sudo curl -LO https://go.dev/dl/go1.23.3.linux-amd64.tar.gz && sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go*.linux-amd64.tar.gz && sudo rm go*.linux-amd64.tar.gz
     ;;
-  t_parse_html)
+  html)
     cd $PROJECT/tools && python parse_html.py
     ;;
-  
+  build)
+    cd $PROJECT/llm/cmd && CGO_ENABLED=1 go build -tags="ggml" -o $PROJECT/bin/llm
+    ;;
   *)
     echo "not support:$1"
     ;;
