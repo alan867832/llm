5bfa3161ff134e3e83787e1e1fec8be4a059d0b5
aa <aa@aa.com>
2025-03-09 12:49:33 +0800

    add

--- opt.sh
+++ opt.sh
@@ -4,7 +4,10 @@ PROJECT=$PWD
 GO=/usr/local/go/bin/go
 case $1 in
   install)
+    sudo apt-get update
+    sudo apt-get install build-essential gcc cmake -y
     sudo curl -LO https://go.dev/dl/go1.23.3.linux-amd64.tar.gz && sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go*.linux-amd64.tar.gz && sudo rm go*.linux-amd64.tar.gz
+    cd tools && python3 -m pip install -r requirements.txt
     ;;
   html)
     cd $PROJECT/tools && python parse_html.py
