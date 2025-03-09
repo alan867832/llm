267620997e97f3dfb582e6fb5a6288b94e0ec264
aa <aa@aa.com>
2025-03-09 12:19:25 +0800

    init project

---
+++ llm/cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"llm/runner"
+	"os"
+)
+
+func main() {
+	if err := runner.Execute(os.Args[1:]); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s\n", err)
+		os.Exit(1)
+	}
+}
